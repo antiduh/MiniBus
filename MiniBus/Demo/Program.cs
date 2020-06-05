57da24cd53653aab69863a88bc096331ff38cfd1
antiduh <antiduh@56f50355-9621-eb11-831a-fdcf2b5adb27>
2020-06-05 03:42:57 +0000

    Wrote a basic echo server-client demo.

--- MiniBus/Demo/Program.cs
+++ MiniBus/Demo/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Windows.Forms;
+using Echo.Client;
+using Echo.Service;
+using RabbitMQ.Client;
 
 namespace Demo
 {
@@ -12,7 +16,55 @@ namespace Demo
         {
             Console.WriteLine( "Demo starting." );
 
+            var conn = new RabbitConn();
+            IModel channel = conn.Connect();
+
+            var service = new EchoService();
+            service.Connect( channel );
+
+
+            var client = new EchoClient( new RabbitClientBus( channel ) );
+            client.DoEcho( "Hello" );
+
+            MessageBox.Show( "Done" );
+
+            conn.Dispose();
+
             Console.WriteLine( "Demo exiting." );
+        }
+
+        private class RabbitConn : IDisposable
+        {
+            private ConnectionFactory rabbitConnFactory;
+
+            private IConnection connection;
+
+            private IModel channel;
+
+            public RabbitConn()
+            {
+                this.rabbitConnFactory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    UserName = "antiduh",
+                    Password = "passwordpassw",
+                    VirtualHost = "sdv-test",
+                };
+            }
+
+            public IModel Connect()
+            {
+                this.connection = rabbitConnFactory.CreateConnection();
+                this.channel = this.connection.CreateModel();
+
+                return this.channel;
+            }
+
+            public void Dispose()
+            {
+                this.channel.Dispose();
+                this.connection.Dispose();
+            }
         }
     }
 }
