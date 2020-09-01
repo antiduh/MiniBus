d524178aa65a223b79fd240608f9fda974d35e6f
antiduh <antiduh@56f50355-9621-eb11-831a-fdcf2b5adb27>
2020-09-01 21:04:26 +0000

    Stubs for making it easier to read/write TLV objects with byte arrays.

---
+++ trunk/MiniBus/MiniBus.Services/TlvBufferReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using PocketTLV;
+using PocketTLV.Primitives;
+
+namespace MiniBus.ServiceApi
+{
+    public class TlvBufferReader : ITlvReader
+    {
+        public ITlvContract ReadContract()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ReadContract<T>() where T : ITlvContract, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITag ReadTag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ReadTag<T>() where T : ITag
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterContract<T>() where T : ITlvContract, new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/MiniBus/MiniBus.Services/TlvBufferWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PocketTLV;
+using PocketTLV.Primitives;
+
+namespace MiniBus.ServiceApi
+{
+    public class TlvBufferWriter : ITlvWriter
+    {
+        public void Write( ITag tag )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write( ITlvContract contract )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
