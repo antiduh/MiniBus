0d761ccfc870e2788211b661824da52d35b88d30
antiduh <antiduh@56f50355-9621-eb11-831a-fdcf2b5adb27>
2020-09-02 18:50:40 +0000

    Created a basic ObjectPool class.

---
+++ trunk/MiniBus/MiniBus/ObjectPool.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBus.ClientApi
+{
+    public class ObjectPool<T>
+    {
+        private readonly ConcurrentBag<T> objects;
+
+        private readonly Func<T> objectGenerator;
+
+        public ObjectPool( Func<T> objectGenerator )
+        {
+            if( objectGenerator == null )
+            {
+                throw new ArgumentNullException( nameof( objectGenerator ) );
+            }
+
+            this.objectGenerator = objectGenerator;
+
+            this.objects = new ConcurrentBag<T>();
+        }
+
+        public T Get()
+        {
+            if( objects.TryTake( out T item ) )
+            {
+                return item;
+            }
+
+            return objectGenerator();
+        }
+
+        public void Return( T item )
+        {
+            objects.Add( item );
+        }
+    }
+}
