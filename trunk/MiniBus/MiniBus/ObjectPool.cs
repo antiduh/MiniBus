d4fa7a85987361c463380c498a1f6f83c9587cae
antiduh <antiduh@56f50355-9621-eb11-831a-fdcf2b5adb27>
2020-09-02 21:57:13 +0000

    Switched to a non-locking ObjectPool since we're going to handle our own concurrency.

---
+++ trunk/MiniBus/MiniBus/ObjectPool.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiniBus
+{
+    public class ObjectPool<T>
+    {
+        private readonly Queue<T> objects;
+
+        private readonly Func<T> objectGenerator;
+
+        public ObjectPool( Func<T> objectGenerator )
+        {
+            if( objectGenerator == null )
+            {
+                throw new ArgumentNullException( nameof( objectGenerator ) );
+            }
+
+            this.objectGenerator = objectGenerator;
+
+            this.objects = new Queue<T>();
+        }
+
+        public T Get()
+        {
+            if( objects.Count > 0 )
+            {
+                return objects.Dequeue();
+            }
+
+            return objectGenerator();
+        }
+
+        public void Return( T item )
+        {
+            objects.Enqueue( item );
+        }
+    }
+}
