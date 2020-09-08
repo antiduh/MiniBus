7516cb88b0e8dd62f44f3aa2d380d720e48d188e
antiduh <antiduh@56f50355-9621-eb11-831a-fdcf2b5adb27>
2020-09-08 20:36:53 +0000

    Spell out 'correlation' for completeness.

--- MiniBus/MiniBus/Envelope.cs
+++ MiniBus/MiniBus/Envelope.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// Specifies a correlation ID to associate with the message. All replies to the message will be tagged with the same correlation ID.
         /// </summary>
-        public string CorrId { get; set; }
+        public string CorrelationId { get; set; }
 
         /// <summary>
         /// Optionally specifies a specific routing key to direct message replies to; ignored by client transports.
