2788ad2b9a5a760f28e56da1dee676ba42f016e7
antiduh <antiduh@56f50355-9621-eb11-831a-fdcf2b5adb27>
2020-11-15 08:45:38 +0000

    Improved the handling of the envelope in the server side.

--- MiniBus.Services/IServerBus.cs
+++ MiniBus.Services/IServerBus.cs
@@ -5,7 +5,7 @@ namespace MiniBus.Services
 {
     public interface IServerBus
     {
-        void SendMessage( Envelope env, ITlvContract msg );
+        void SendMessage( ServerEnvelope env, ITlvContract msg );
 
         void RegisterHandler<T>( Action<T, IConsumeContext> handler, string queueName ) where T : ITlvContract, new();
 
