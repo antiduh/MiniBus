f63bd2b9cfa174cf9f1d767bec125fd64d12f60f
antiduh <antiduh@56f50355-9621-eb11-831a-fdcf2b5adb27>
2020-11-16 06:31:17 +0000

    Working on adding specific exceptions to the library.

---
+++ trunk/MiniBus/DeliveryException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MiniBus
+{
+    /// <summary>
+    /// Represents errors that occur while trying to transmit and receive messages. <see
+    /// cref="DeliveryException"/> is the base class for all MiniBus communication exceptions.
+    /// </summary>
+    public class DeliveryException : Exception
+    {
+        public DeliveryException()
+        {
+        }
+
+        public DeliveryException( string message ) : base( message )
+        {
+        }
+
+        public DeliveryException( string message, Exception innerException ) : base( message, innerException )
+        {
+        }
+
+        protected DeliveryException( SerializationInfo info, StreamingContext context ) : base( info, context )
+        {
+        }
+    }
+}
---
+++ trunk/MiniBus/TimeoutException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MiniBus
+{
+    /// <summary>
+    /// Occurs when a timeout has occurred while waiting to recieve a message.
+    /// </summary>
+    public class TimeoutException : DeliveryException
+    {
+        public TimeoutException() : this( "A timeout occurred while waiting for a message." )
+        {
+        }
+
+        public TimeoutException( string message ) : base( message )
+        {
+        }
+
+        public TimeoutException( string message, Exception innerException ) : base( message, innerException )
+        {
+        }
+
+        protected TimeoutException( SerializationInfo info, StreamingContext context ) : base( info, context )
+        {
+        }
+    }
+}
