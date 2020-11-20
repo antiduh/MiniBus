2cde1e43f3c7cc8ceaf39c26cbce46af2c10d643
antiduh <antiduh@56f50355-9621-eb11-831a-fdcf2b5adb27>
2020-11-20 04:25:53 +0000

    Moved the child class for ClientSession to its own file.

---
+++ Gateway.Service/GatewayService.ClientSession.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net.Sockets;
+using MiniBus;
+using MiniBus.Gateway;
+using PocketTlv;
+
+namespace Gateway.Service
+{
+    public partial class GatewayService
+    {
+        private class ClientSession
+        {
+            private readonly TcpClient client;
+            private readonly GatewayService parent;
+
+            private MiniBusTlvClient tlvSocket;
+
+            public ClientSession( TcpClient client, GatewayService parent )
+            {
+                this.client = client;
+                this.parent = parent;
+                this.ClientId = CorrId.Create();
+
+                this.tlvSocket = new MiniBusTlvClient( client.GetStream() );
+                this.tlvSocket.Register<GatewayRequestMsg>();
+                this.tlvSocket.Register<GatewayHeartbeatRequest>();
+                this.tlvSocket.Received += Socket_Received;
+            }
+
+            public string ClientId { get; private set; }
+
+            public void Start()
+            {
+                this.tlvSocket.Start();
+            }
+
+            public void Stop()
+            {
+            }
+
+            public void Write( ITlvContract message )
+            {
+                this.tlvSocket.SendMessage( message );
+            }
+
+            private void Socket_Received( ITlvContract tlvContract )
+            {
+                if( tlvContract.ContractId == GatewayTlvs.EchoRequest )
+                {
+                    Write( new GatewayHeartbeatResponse() );
+                }
+                else
+                {
+                    GatewayRequestMsg msg;
+                    if( tlvContract.TryResolve( out msg ) == false )
+                    {
+                        return;
+                    }
+
+                    // We received a message from the client. Forward it to rabbit.
+
+                    if( false )
+                    {
+                        Console.WriteLine( "Gateway received message: " );
+                        Console.WriteLine( " - Client UID:  " + this.ClientId );
+                        Console.WriteLine( " - RoutingKey:  " + msg.RoutingKey );
+                        Console.WriteLine( " - MessageName: " + msg.MessageName );
+                        Console.WriteLine();
+                    }
+
+                    parent.PublishRabbit( msg, this );
+                }
+            }
+        }
+    }
+}
--- Gateway.Service/GatewayService.cs
+++ Gateway.Service/GatewayService.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using MiniBus;
 using MiniBus.Gateway;
 using MiniBus.Services;
 using PocketTlv;
@@ -13,7 +12,7 @@ using RabbitMQ.Client.Events;
 
 namespace Gateway.Service
 {
-    public class GatewayService
+    public partial class GatewayService
     {
         private readonly int port;
 
@@ -144,71 +143,6 @@ namespace Gateway.Service
                 catch( SocketException e )
                 {
                     break;
-                }
-            }
-        }
-
-        private class ClientSession
-        {
-            private readonly TcpClient client;
-            private readonly GatewayService parent;
-
-            private MiniBusTlvClient tlvSocket;
-
-            public ClientSession( TcpClient client, GatewayService parent )
-            {
-                this.client = client;
-                this.parent = parent;
-                this.ClientId = CorrId.Create();
-
-                this.tlvSocket = new MiniBusTlvClient( client.GetStream() );
-                this.tlvSocket.Register<GatewayRequestMsg>();
-                this.tlvSocket.Register<GatewayHeartbeatRequest>();
-                this.tlvSocket.Received += Socket_Received;
-            }
-
-            public string ClientId { get; private set; }
-
-            public void Start()
-            {
-                this.tlvSocket.Start();
-            }
-
-            public void Stop()
-            {
-            }
-
-            public void Write( ITlvContract message )
-            {
-                this.tlvSocket.SendMessage( message );
-            }
-
-            private void Socket_Received( ITlvContract tlvContract )
-            {
-                if( tlvContract.ContractId == GatewayTlvs.EchoRequest )
-                {
-                    Write( new GatewayHeartbeatResponse() );
-                }
-                else
-                {
-                    GatewayRequestMsg msg;
-                    if( tlvContract.TryResolve( out msg ) == false )
-                    {
-                        return;
-                    }
-
-                    // We received a message from the client. Forward it to rabbit.
-
-                    if( false )
-                    {
-                        Console.WriteLine( "Gateway received message: " );
-                        Console.WriteLine( " - Client UID:  " + this.ClientId );
-                        Console.WriteLine( " - RoutingKey:  " + msg.RoutingKey );
-                        Console.WriteLine( " - MessageName: " + msg.MessageName );
-                        Console.WriteLine();
-                    }
-
-                    parent.PublishRabbit( msg, this );
                 }
             }
         }
