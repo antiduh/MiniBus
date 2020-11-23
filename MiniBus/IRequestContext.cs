8c6fe5244c0d5eb9ba73bd4a8af60f99f9223448
antiduh <antiduh@56f50355-9621-eb11-831a-fdcf2b5adb27>
2020-11-23 22:52:31 +0000

    Moved classes to their own files.

---
+++ MiniBus/IRequestContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PocketTlv;
+
+namespace MiniBus
+{
+    public interface IRequestContext : IDisposable
+    {
+        void SendRequest( ITlvContract msg );
+
+        ITlvContract WaitResponse( TimeSpan timeout );
+
+        T WaitResponse<T>( TimeSpan timeout ) where T : ITlvContract, new();
+
+        void WithRetry( Action action );
+    }
+}
